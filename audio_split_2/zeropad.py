52a151206ef6996ac21d5bae37e033eb62b71436
ara <adhikara@mit.edu>
2016-08-22 17:27:08 -0400

    removed audio files

--- audio_split_2/zeropad.py
+++
@@ -1,9 +0,0 @@
-import os
-path = './'
-for filename in os.listdir(path):
-    if filename.endswith('.wav'):
-	    print(filename)
-	    prefix1, prefix2, num = filename[:-4].split('_')
-	    num = num.zfill(3)
-	    new_filename = prefix1 + "_" + prefix2 + "_" + num + "_conv.wav"
-	    os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
